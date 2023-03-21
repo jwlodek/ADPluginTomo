89d7fe92b9e7125f462f5839ffc0f193cae16e33
Jakub Wlodek <jwlodek@bnl.gov>
2023-03-21 11:49:19 -0400

    Updated client example - tested & working

--- scripts/simple-client-example.py
+++ scripts/simple-client-example.py
@@ -1,28 +1,53 @@
 #!/usr/bin/env python3
 
-#import cv2
+import cv2
 import socket
-#import numpy as np
-
+import numpy as np
+import struct
 PROTOCOL_HEADER_SIZE=48
 
 
 class NSLS2TomoStreamProtocolHeader:
 
     def __init__(self, raw_header_data):
-        self.protocol_version = f'{int(raw_header_data[0])}.{raw_header_data[1]}.{raw_header_data[2]}'
-        self.frame_type = raw_header_data[3]
-        self.reference_type = raw_header_data[4]
-        self.reference = raw_header_data[5:13]
-        self.num_bytes = raw_header_data[14:22]
-        self.x_size = int.from_bytes(raw_header_data[23:31], "little")
-        self.y_size = int.from_bytes(raw_header_data[32:39], "little")
-        self.color_channels = raw_header_data[40:47]
+        print(raw_header_data)
+        unpacked = struct.unpack("BBBBBBdNNNN", raw_header_data)
+        print(unpacked)
+
+        self.protocol_version = f'{unpacked[0]}.{unpacked[1]}.{unpacked[2]}'
+        self.frame_type = unpacked[3]
+        self.reference_type = unpacked[4]
+        self._dtype = unpacked[5]
+        self.reference = unpacked[6]
+        self.num_bytes =unpacked[7]
+        self.x_size = unpacked[8]
+        self.y_size = unpacked[9]
+        self.color_channels = unpacked[10]
+
 
     def __str__(self):
+        
         to_str = f'NSLS2 Tomo Protocol Header {self.protocol_version}\n'
-        to_str += f'{self.frame_type} frame, w/ dims {self.x_size}x{self.y_size}'
+        to_str += f'{self.frame_type} frame, ref: {self.reference} w/ dims {self.x_size}x{self.y_size}'
         return to_str
+
+
+    @property
+    def dtype(self):
+
+        if self._dtype == 0:
+            return np.dtype('int8')
+        elif self._dtype == 1:
+            return np.dtype('uint8')
+        elif self._dtype == 2:
+            return np.dtype('int16')
+        elif self._dtype == 3:
+            return np.dtype('uint16')
+
+
+            
+       
+
 
 def client():
     host = "127.0.0.1"  # as both code is running on same pc
@@ -38,9 +63,21 @@ def client():
 
         header = NSLS2TomoStreamProtocolHeader(data)
 
+        print(str(header.dtype))
+
         print(str(header))
 
-        #raw_img_data = client_socket.recv(header.num_bytes)
+        arr = bytearray()
+        while len(arr) < header.num_bytes:
+            arr += client_socket.recv(header.num_bytes)
+        img_data = arr[:header.num_bytes]
+        print(len(img_data))
+        for i in range(10):
+            start = i * 2
+            print(f"Pixel value of pixel {i}: {int.from_bytes(img_data[start:start+1], 'little')}")
+
+        img = np.frombuffer(img_data, dtype=header.dtype).reshape((header.x_size, header.y_size, 1))
+        cv2.imshow('Test', img)
 
 
 
